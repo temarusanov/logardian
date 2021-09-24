9d70071497ad0bbb262885f14a50a9c86172fc55
temz <artem.rusanov.02@gmail.com>
2021-09-24 12:27:21 +0700

    fix: production log didnt show metadata

--- lib/helpers/create-winston-logger.helper.ts
+++ lib/helpers/create-winston-logger.helper.ts
@@ -1,4 +1,6 @@
-/* eslint-disable */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-require-imports */
 require('dotenv').config()
 import { Logger, format, transports, createLogger } from 'winston'
 import * as chalk from 'chalk'
@@ -8,42 +10,60 @@ import { colorizeContext } from './colorize-context.helper'
 const JSON_SPACE = 2
 const isProduction = process.env.NODE_ENV === 'production'
 
-const { json, combine, timestamp, printf, colorize, metadata } = format
+const { combine, timestamp, printf, colorize } = format
 
-const level = isProduction ? 'info' : 'debug'
-const consoleFormat = isProduction
-    ? createWinstonFormatForProduction()
-    : createWinstonFormatForDevelopment()
+const colorizer = colorize()
 
 export const createWinstonLogger = (): Logger => {
+    const level = isProduction ? 'info' : 'debug'
+
     const output = createLogger({ level })
+
+    const consoleFormat = createWinstonFormat()
 
     output.add(new transports.Console({ format: consoleFormat }))
 
     return output
 }
 
-function createWinstonFormatForProduction(): Format {
+function createWinstonFormat(): Format {
     return combine(
-        timestamp(),
-        metadata({
-            fillExcept: ['message', 'level', 'timestamp', 'label'],
-        }),
-        json({ space: JSON_SPACE }),
-    )
-}
-
-function createWinstonFormatForDevelopment(): Format {
-    return combine(
-        colorize(),
         timestamp(),
         format.splat(),
         format.simple(),
-        printf((data) => createLine(data)),
+        printf((info) =>
+            isProduction
+                ? createProductionLog(info)
+                : createDevelopmentLog(info),
+        ),
     )
 }
 
-function createLine(info: any): string {
+function createProductionLog(info: any): string {
+    const {
+        timestamp,
+        message,
+        level,
+        label,
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        trace,
+        ...meta
+    } = info
+
+    return JSON.stringify(
+        {
+            timestamp,
+            message: getMessage(message),
+            level,
+            label,
+            metadata: getMetaForProduction(meta),
+        },
+        null,
+        JSON_SPACE,
+    )
+}
+
+function createDevelopmentLog(info: any): string {
     const {
         timestamp,
         message,
@@ -51,23 +71,28 @@ function createLine(info: any): string {
         label,
         caller,
         path,
-        levelColor,
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
         trace,
         ...meta
     } = info
 
-    return `${getTime(timestamp)} ${level}: ${getLabel(label)}${getMessage(
-        message,
-    )}${getMeta(meta)}${getExecutorValues(caller, path, trace)}`
+    return `${getTime(timestamp)} ${getLevel(level)}: ${getLabel(
+        label,
+    )}${getMessage(message)}${getMeta(meta)}${getExecutorValues(caller, path)}`
 }
 
 function getTime(timestamp: string): string {
     const time = new Date(timestamp).toISOString()
 
+    // eslint-disable-next-line @typescript-eslint/no-magic-numbers
     return `${chalk.cyan(`[${`${time.substr(0, 10)} ${time.substr(11, 8)}`}]`)}`
 }
 
-function getMessage(message: string) {
+function getLevel(level: string): string {
+    return colorizer.colorize(level, level)
+}
+
+function getMessage(message: string): string {
     return typeof message === 'string'
         ? message
         : `\n${JSON.stringify(message, null, JSON_SPACE)}`
@@ -77,15 +102,7 @@ function getLabel(label: string): string {
     return `${label ? colorizeContext(label, '[' + label + '] ') : ''}`
 }
 
-function getExecutorValues(
-    caller: string,
-    path: string,
-    trace: boolean,
-): string {
-    if (!trace) {
-        return ``
-    }
-
+function getExecutorValues(caller: string, path: string): string {
     return `${caller ? `\ncaller ${chalk.gray(`->`)} ${caller}` : ''}${
         path ? `\npath ${chalk.gray(`->`)} ${path}` : ``
     }`
@@ -109,4 +126,12 @@ function getMeta(meta: any): string {
     }
 
     return ``
+}
+
+function getMetaForProduction(meta: any): any {
+    if (typeof meta[0] === 'string' && meta[0].length === 1) {
+        return {}
+    }
+
+    return meta
 }
